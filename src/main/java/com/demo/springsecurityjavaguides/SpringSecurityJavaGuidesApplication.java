70ba6277448c8bec32cd1bca3ad3eadd828902ea
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-27 18:33:25 +0300

    Initial commit

---
+++ src/main/java/com/demo/springsecurityjavaguides/SpringSecurityJavaGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springsecurityjavaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJavaGuidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJavaGuidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springsecurityjavaguides/SpringSecurityJavaGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springsecurityjavaguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJavaGuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
