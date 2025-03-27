76206c8479fa9fce21e4e59e8c1a3319aa754811
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-28 00:17:13 +0300

    create User and Role entities

---
+++ src/main/java/com/demo/springsecurityjavaguides/entity/Role.java
@@ -0,0 +1,45 @@
+package com.demo.springsecurityjavaguides.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Collection;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users;
+
+    public Role() {
+    }
+
+    public Role(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
