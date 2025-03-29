f12f3bf72f687aa9a4c205b2d577181f69fe4d22
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-03-29 05:09:07 +0300

    modify SecurityConfig

--- src/main/java/com/demo/springsecurityjavaguides/config/SecurityConfig.java
+++ src/main/java/com/demo/springsecurityjavaguides/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.demo.springsecurityjavaguides.config;
 
 import com.demo.springsecurityjavaguides.entity.User;
 import com.demo.springsecurityjavaguides.security.CustomUserDetailsService;
+import com.demo.springsecurityjavaguides.security.JwtAuthenticationEntryPoint;
+import com.demo.springsecurityjavaguides.security.JwtAuthenticationFilter;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,15 +21,22 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(UserDetailsService userDetailsService) {
+    public SecurityConfig(UserDetailsService userDetailsService,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAuthenticationFilter jwtAuthenticationFilter) {
         this.userDetailsService = userDetailsService;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
@@ -46,7 +55,11 @@ public class SecurityConfig {
                     request.requestMatchers("/api/test/user/**").hasAnyRole("ADMIN", "USER");
                     request.requestMatchers("/api/test/admin/**").hasRole("ADMIN");
                 })
-                .httpBasic(Customizer.withDefaults());
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
